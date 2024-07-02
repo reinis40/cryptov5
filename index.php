c2c5a37b2dceac5064ce553df477162f3b29deed
reinis40 <88657385+reinis40@users.noreply.github.com>
2024-07-02 18:12:16 +0300

    index.php

--- index.php
+++ index.php
@@ -17,32 +17,30 @@ use FastRoute\RouteCollector;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$apiKey = $_ENV['API_KEY'];
 
+$dbFile = 'storage/database.sqlite';
+$pdo = new PDO('sqlite:' . $dbFile);
 
+$user = new User($pdo);
+$user->id = 1;
 
 $loader = new FilesystemLoader(__DIR__ . '/views');
 $twig = new Environment($loader, [
       'cache' => false,
 ]);
 
-$dbFile = 'storage/database.sqlite';
-$pdo = new PDO('sqlite:' . $dbFile);
-$user = new User($pdo);
-$user->id = 1;
 $logger = new TransactionLogger($dbFile);
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-$apiKey = $_ENV['API_KEY'];
+
 $api = new CoinMarketCapApi($apiKey);
 $cryptoManager = new CryptoManager($api, $pdo, $user, $logger);
-$cryptoController = new CryptoController( $cryptoManager,$twig);
-
-
+$cryptoController = new CryptoController($cryptoManager, $twig);
 
 $dispatcher = FastRoute\simpleDispatcher(function(RouteCollector $r) {
     $routes = include('routes.php');
-    foreach ($routes as $route)
-    {
+    foreach ($routes as $route) {
         [$method, $url, $controller] = $route;
         $r->addRoute($method, $url, $controller);
     }
@@ -71,45 +69,19 @@ switch ($routeInfo[0]) {
         $handler = $routeInfo[1];
         $vars = $routeInfo[2];
 
-        [$controller, $method] = $handler;
-        echo $cryptoController->$method();
+        [$controllerClass, $method] = $handler;
+        $controller = new $controllerClass($cryptoManager, $twig);
+        if (method_exists($controller, $method)) {
+            $response = $controller->$method();
+            if (isset($response['template'], $response['data'])) {
+                echo $twig->render($response['template'], $response['data']);
+            }
+        }
         break;
 }
-//echo "Login:\n";
-//$username = readline("Username: ");
-//$password = readline("Password: ");
-//if (!$user->login($username, $password)) {
-//    exit("Invalid username or password.\n");
-//}
-//while (true) {
-//    echo "\n1. List of crypto\n2. Buy\n3. Sell\n4. View wallet\n5. View logs\n6. Exit\n";
-//    $input = readline("Select an option: ");
-//
-//    switch ($input) {
-//        case 1:
-//            $cryptoManager->showCrypto();
-//            break;
-//        case 2:
-//            $symbol = readline("Enter cryptocurrency symbol: ");
-//            $amountEUR = readline("Enter amount in EUR to buy: ");
-//            $cryptoManager->buyCrypto(strtoupper($symbol), (float)$amountEUR);
-//            break;
-//        case 3:
-//            $symbol = readline("Enter cryptocurrency symbol: ");
-//            $cryptoManager->sellCrypto(strtoupper($symbol));
-//            break;
-//        case 4:
-//            $cryptoManager->showWallet();
-//            break;
-//        case 5:
-//            $logger->showTransactions();
-//            break;
-//        case 6:
-//            exit("Goodbye!\n");
-//        default:
-//            echo "Invalid option, please try again.\n";
-//    }
-//}
+
+
+
 
 
 
